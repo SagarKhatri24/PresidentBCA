d689854978c227c6c16ffcbcbd785658f9f637c6
Sagar Khatri <khatrisagar2@gmail.com>
2025-09-10 13:00:27 +0530

    ListView

--- app/src/main/java/president/bca/MainActivity.java
+++ app/src/main/java/president/bca/MainActivity.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.GridView;
+import android.widget.ListView;
+import android.widget.MultiAutoCompleteTextView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -13,10 +17,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     Spinner spinner;
-    String[] countryArray = {"India","Australia","UK","USA","Canada"};
+    String[] countryArray = {"Select Country","India","Australia","UK","USA","Canada"};
+    ArrayList<String> arrayList;
+
+    GridView listView;
+
+    AutoCompleteTextView autoTxt;
+
+    MultiAutoCompleteTextView multiAutoTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,14 +38,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         spinner = findViewById(R.id.main_spinner);
-        ArrayAdapter adapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1,countryArray);
+
+        arrayList = new ArrayList<>();
+        arrayList.add("Select Country");
+        arrayList.add("China");
+        arrayList.add("USA");
+        arrayList.add("UK");
+        arrayList.add("Test");
+        arrayList.add("Canda");
+
+        arrayList.remove(4);
+        arrayList.set(4,"Canada");
+
+        arrayList.add(1,"India");
+
+        ArrayAdapter adapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1,arrayList);
         adapter.setDropDownViewResource(android.R.layout.simple_list_item_checked);
         spinner.setAdapter(adapter);
 
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(MainActivity.this, countryArray[i], Toast.LENGTH_SHORT).show();
+                if(i==0){
+
+                }
+                else {
+                    //Toast.makeText(MainActivity.this, countryArray[i], Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, arrayList.get(i), Toast.LENGTH_SHORT).show();
+                }
             }
 
             @Override
@@ -40,6 +73,34 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        listView = findViewById(R.id.listview);
+        ArrayAdapter listAdapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1,arrayList);
+        listView.setAdapter(listAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                if(i==0){
+
+                }
+                else {
+                    //Toast.makeText(MainActivity.this, countryArray[i], Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, arrayList.get(i), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        autoTxt = findViewById(R.id.main_auto);
+        ArrayAdapter autoAdapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1,arrayList);
+        autoTxt.setThreshold(1);
+        autoTxt.setAdapter(autoAdapter);
+
+        multiAutoTxt = findViewById(R.id.main_multi_auto);
+        ArrayAdapter multiautoAdapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1,arrayList);
+        multiAutoTxt.setThreshold(1);
+        multiAutoTxt.setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());
+        multiAutoTxt.setAdapter(multiautoAdapter);
 
     }
 }
